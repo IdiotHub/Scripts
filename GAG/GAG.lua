7170ae180b6de3fbd9b6572ef0fd08a3a6ceeb8b
Somerandomidiot <90102713+Somerandomidiot@users.noreply.github.com>
2025-06-17 13:36:35 +0545

    Update GAG.lua

--- GAG/GAG.lua
+++ GAG/GAG.lua
@@ -1,3 +1,3 @@
 for _, x  in pairs(getconnections(game.Players.LocalPlayer.Idled)) do x:Disable() end 
 
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/13483590b9ced96e"))()
+loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/2b9dc1cfe9cccaa2"))()
