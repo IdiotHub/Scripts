f57c8cc22e3c0164cae91f1ccb080ab560eb446d
vkz. <lucaspedgley1@gmail.com>
2025-06-21 22:17:44 +0300

    Update GAG.lua

--- GAG/GAG.lua
+++ GAG/GAG.lua
@@ -1,7 +1,3 @@
 for _, x  in pairs(getconnections(game.Players.LocalPlayer.Idled)) do x:Disable() end 
-
-task.spawn(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/ployprimetime/Spawner/refs/heads/main/script.lua"))() end)
-
-
 loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/e2d99426ee175e86"))()
 
