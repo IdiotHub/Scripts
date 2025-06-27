2eeff882d071982c2570a7b1d20cf56158f99e6c
Somerandomidiot <90102713+Somerandomidiot@users.noreply.github.com>
2025-06-27 09:16:08 +0545

    Update main.lua

--- BGSI/main.lua
+++ BGSI/main.lua
@@ -1,3 +1,3 @@
 getgenv().DISABLE_RAYFIELD_REQUESTS = true
 
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/232a0d231c88a9df"))()
+loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/b0e8d3bb77e1eb0a"))()
