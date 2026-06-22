b21a0557cd985a2bc8f979616063342559f6cca4
Somerandomidiot <90102713+Somerandomidiot@users.noreply.github.com>
2026-06-23 00:57:26 +0545

    Update UI_FREE.lua

--- GAG2/UI_FREE.lua
+++ GAG2/UI_FREE.lua
@@ -21,4 +21,5 @@ if isShiityExec() then
     game.Players.LocalPlayer:Kick("Hi, IdiotHub is not supported by your exec.")
     return
 end
+
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/bda9fc8956f2d07e60b36d6d7c772bc23456c51ce3faad3e9fb9af07c5b6a0d8/download"))()
