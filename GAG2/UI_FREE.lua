d46b94b3794d155b1bcc776eb491fef271ca13b0
Somerandomidiot <90102713+Somerandomidiot@users.noreply.github.com>
2026-06-22 12:42:29 +0545

    Implement execution environment check
    Added a function to check for supported execution environment and kick unsupported clients.

--- GAG2/UI_FREE.lua
+++ GAG2/UI_FREE.lua
@@ -1,2 +1,24 @@
 getgenv().SCRIPT_KEY = "KEYLESS"
+function isShiityExec()
+    if not getgenv then return true end
+    if not debug then return true end 
+    local debugChecks = {
+        "getupvalue", 
+        "setupvalue",
+        "info",
+    }
+    
+    for _, func in pairs(debugChecks) do 
+        if not debug[func] then return true end
+    end
+    
+    if not getconnections then return true end
+    
+    return false
+end
+
+if isShiityExec() then 
+    game.Players.LocalPlayer:Kick("Hi, IdiotHub is not supported by your exec.")
+    return
+end
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/bda9fc8956f2d07e60b36d6d7c772bc23456c51ce3faad3e9fb9af07c5b6a0d8/download"))()
