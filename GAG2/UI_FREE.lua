7e53f7c872f3d4e72095e6deca2725f8a4d412b1
Somerandomidiot <90102713+Somerandomidiot@users.noreply.github.com>
2026-06-26 12:59:22 +0545

    Update UI_FREE.lua

--- GAG2/UI_FREE.lua
+++ GAG2/UI_FREE.lua
@@ -1,25 +1,28 @@
-getgenv().SCRIPT_KEY = "KEYLESS"
+
+getgenv().SCRIPT_KEY = "24c8d586-0bcd-4882-a884-835761c287f7"
+
 function isShiityExec()
     if not getgenv then return true end
-    if not debug then return true end 
+    if not debug then return true end
+
     local debugChecks = {
-        "getupvalue", 
+        "getupvalue",
         "setupvalue",
         "info",
     }
-    
-    for _, func in pairs(debugChecks) do 
+
+    for _, func in pairs(debugChecks) do
         if not debug[func] then return true end
     end
-    
+
     if not getconnections then return true end
-    
+
     return false
 end
 
-if isShiityExec() then 
+if isShiityExec() then
     game.Players.LocalPlayer:Kick("Hi, IdiotHub is not supported by your exec.")
     return
 end
 
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/bda9fc8956f2d07e60b36d6d7c772bc23456c51ce3faad3e9fb9af07c5b6a0d8/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/bda9fc8956f2d07e60b36d6d7c772bc23456c51ce3faad3e9fb9af07c5b6a0d8/download"))() 
